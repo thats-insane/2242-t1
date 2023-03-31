9574b51c4c15ea3d427b4f962bd83dc09457e71e
krismic <2021154337@ub.edu.bz>
2023-03-31 12:58:51 -0600

    done with the home and greeting handlers, starting the random handler

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	w.Write([]byte("Howdy. My name is Cahlil and I like playing Yu-Gi-Oh! and I hate women. My e-mail address is 2021154337@ub.edu.bz if you ever want to contact me."))
 }
 
 func Greeting(w http.ResponseWriter, r *http.Request) {
-	time = time.Now()
-	day := time.Weekday().string()
-	greeting := "Right now it is" + time + ". Enjoy the rest of your" + day + "."
+	greeting := "Right now it is" + time.Now().Format(time.Kitchen) + ". Enjoy the rest of your" + time.Now().Weekday().String() + "."
+
 	w.Write([]byte(greeting))
 }
 
-type Random struct {
-	quote string
+func Random(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
+	log.Fatal(http.ListenAndServe(":4000", nil))
 }
